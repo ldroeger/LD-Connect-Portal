6451a396c260ecf0d92c89701b416321fea71c38
l.droeger <lukas@droeger.org>
2026-08-07 13:34:05 +0200

    Fix: Caddy Entrypoint korrektes Caddyfile Format

--- caddy/caddy-entrypoint.sh
+++ caddy/caddy-entrypoint.sh
@@ -1,27 +1,12 @@
 #!/bin/sh
 set -e
 
-# Verzeichnis anlegen
 mkdir -p /app-data/caddy
 
-# Standard-Caddyfile anlegen wenn noch nicht vorhanden
 if [ ! -f /app-data/caddy/Caddyfile ]; then
   echo "Erstelle initiales Caddyfile..."
-  cat > /app-data/caddy/Caddyfile << 'CADDYEOF'
-{
-    auto_https off
-}
-
-:80 {
-    encode gzip
-    request_body {
-        max_size 200MB
-    }
-    reverse_proxy frontend:80
-}
-CADDYEOF
+  printf '%s\n' '{' '    auto_https off' '}' '' ':80 {' '    encode gzip' '    request_body {' '        max_size 200MB' '    }' '    reverse_proxy frontend:80' '}' > /app-data/caddy/Caddyfile
   echo "Caddyfile erstellt."
 fi
 
-# Caddy starten
 exec caddy run --config /app-data/caddy/Caddyfile --adapter caddyfile --watch
